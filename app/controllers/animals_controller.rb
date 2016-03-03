19149fb2d9223a1c16d0c8f8eddf0916811da586
eaud <derek.s.mueller@gmail.com>
2016-03-02 20:28:23 -0500

    Reset API URL

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,11 +1,11 @@
 class AnimalsController < ApplicationController
   def index
-    response = HTTParty.get('http://localhost:3000/api/v1/animals/').parsed_response["animals"]
+    response = HTTParty.get('http://animal-api.herokuapp.com/api/v1/animals/').parsed_response["animals"]
     @animals = Animal.new_from_json(response).shuffle
   end
 
   def environment
-    response = HTTParty.get('http://localhost:3000/api/v1/animals/').parsed_response["animals"]
+    response = HTTParty.get('http://animal-api.herokuapp.com/api/v1/animals/').parsed_response["animals"]
     Animal.new_from_json(response)
     @environments = Animal.sort_by_environment
   end
